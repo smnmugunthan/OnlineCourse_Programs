27baccc32976c5ffad0eb6cf255d99cd762eeb64
Mugunthan S <smnmugunthan@outlook.com>
2020-08-19 17:34:57 +0530

    done

--- Module4_Solution/SpeakGoodBye.js
+++ Module4_Solution/SpeakGoodBye.js
@@ -1,9 +1,9 @@
-((function (window) {
-var byeSpeaker = {};
-var speakWord = "Good Bye";
-byeSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-}
-window.byeSpeaker = byeSpeaker;
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
 
 })(window);
--- Module4_Solution/script.js
+++ Module4_Solution/script.js
@@ -1,16 +1,16 @@
-(function (window) {
+(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-
   var firstLetter = names[i].charAt(0).toLowerCase();
 
-  if (firstLetter == 'j') {
-    window.byeSpeaker.speak(names[i]);
-  } else {
-    window.helloSpeaker.speak(names[i]);
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
   }
 }
 
-})(window);
+})();
